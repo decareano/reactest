e09a2e84a1174b5d9d8ce06ed4bbc5dfa5ad1955
EC2 Default User <ec2-user@ip-10-0-0-145.ec2.internal>
2021-07-16 22:56:55 +0000

    initial commit for react plus firebase

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+//import firebase from "firebase/app"
+const config = {
+    apiKey: "AIzaSyA4QGUX9E72rrKdHVdpEb1Lrz9zwXjPdRc",
+    authDomain: "reactandfirebase-de374.firebaseapp.com",
+    databaseURL: "https://reactandfirebase-de374-default-rtdb.firebaseio.com",
+   
+  };
+  // Initialize Firebase
+ //firebase.initializeApp(config);
+ export default config
